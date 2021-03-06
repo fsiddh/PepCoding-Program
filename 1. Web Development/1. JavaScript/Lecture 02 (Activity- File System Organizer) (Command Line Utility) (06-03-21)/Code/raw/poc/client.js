7d27a8f3a5e430c42a152e620c72dbeeec191f54
fsiddh <jasonshaw546@gmail.com>
2021-03-06 21:13:26 +0530

    notes updated

--- 1. Web Development/1. JavaScript/Lecture 02 (Activity- File System Organizer) (Command Line Utility) (06-03-21)/Code/raw/poc/client.js
+++ 1. Web Development/1. JavaScript/Lecture 02 (Activity- File System Organizer) (Command Line Utility) (06-03-21)/Code/raw/poc/client.js
@@ -1,5 +1,9 @@
 // use 
 // /nodejs
+
+// We had exported a var and a function from lib.js file.
+// Therefore in "libfileObj" var we recieve those info. in the form of an object! 
+
 let libfileObj = require("./lib.js");
 console.log("Inside client");
 console.log(libfileObj.val)
--- 1. Web Development/1. JavaScript/Lecture 02 (Activity- File System Organizer) (Command Line Utility) (06-03-21)/Code/raw/poc/lib.js
+++ 1. Web Development/1. JavaScript/Lecture 02 (Activity- File System Organizer) (Command Line Utility) (06-03-21)/Code/raw/poc/lib.js
@@ -1,3 +1,12 @@
+// In projects for reusibility purposes many times various files require diff. vars
+// and functions. Using "Module.export"  we can achieve such feat.
+
+// Although exporting a whole file isn't possible, fragments(vars, funstions) of a file 
+// can only be exported.
+
+// Here we export a var "a" and a function fn.
+// Which we recieve in client.js file 
+
 let a = 10;
 function fn() {
     console.log("Hello");
