5f79f4ae689b8dd171ba589bbab23e353cb95a41
fsiddh <jasonshaw546@gmail.com>
2021-03-06 20:24:44 +0530

    notes saved

--- 1. Web Development/1. JavaScript/02. Lecture 2 (Activity- File System Organizer) (Command Line Utility) (06-03-21)/Code/activity/mycli.js
+++ 1. Web Development/1. JavaScript/02. Lecture 2 (Activity- File System Organizer) (Command Line Utility) (06-03-21)/Code/activity/mycli.js
@@ -1,18 +1,20 @@
 // view
-// tree
-// flat
+    // tree
+    // flat
 // organize
 // help
-let input = process.argv.slice(2);
-// console.log(input);
-// console.log(input);
-let cmd = input[0]
-switch (cmd) {
+
+let input = process.argv.slice(2); // Jab apn input dalnge (eg."node mycli.js help")to apne input
+                                   // var me after mycli.js sare words aa jaenge .
+
+let cmd = input[0] // based on our command we'll get view/organize/help
+switch (cmd) { // acc. to the cmd we'll enter the case
     case "view":
         //view as tree
         // view as flat 
         // recursion
-        view(input[1], input[2]);
+        view(input[1], input[2]); //whatever be the input tree or flat, view fn() will
+                                  // get called and then acc. it'll get executed
         break;
     case "organize":
         console.log("organize command executed");
@@ -29,7 +31,8 @@ switch (cmd) {
         console.log("Wrong command . Type help to see the list of all the commands");
 }
 
-
+// Upon entering "view" cmd flat or tree will get passed here 
+// and their resp. condition will get executed.
 function view(dirname, mode) {
 
 
