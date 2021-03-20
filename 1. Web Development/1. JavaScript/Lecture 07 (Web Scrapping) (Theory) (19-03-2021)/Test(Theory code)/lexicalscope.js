ffc310f6cc625f51126f056fa33b6ef205ab4ae2
fsiddh <jasonshaw546@gmail.com>
2021-03-20 16:21:18 +0530

    theory update

--- 1. Web Development/1. JavaScript/Lecture 07 (Web Scrapping) (Theory) (19-03-2021)/Test(Theory code)/lexicalscope.js
+++ 1. Web Development/1. JavaScript/Lecture 07 (Web Scrapping) (Theory) (19-03-2021)/Test(Theory code)/lexicalscope.js
@@ -1,16 +1,20 @@
 let myVar = 10;
-function c() {
-    console.log("6", myVar);
-}
+
+console.log("12", myVar);
+
 function b() {
-    // let myVar = 20;
+    // console.log("2", myVar);
+    let myVar = 20;
     console.log("8", myVar);
+    function c() {
+        console.log("6", myVar);
+    }
     c();
     console.log("10", myVar);
 }
 console.log("12", myVar);
 function a() {
-    // let myVar = 30;
+    let myVar = 30;
     console.log("15", myVar);
     b();
     console.log("17", myVar);
