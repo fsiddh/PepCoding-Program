9f623c1cbad854741e93f14f725078f2757ef65f
fsiddh <jasonshaw546@gmail.com>
2021-03-20 11:29:20 +0530

    potm update

---
+++ 1. Web Development/1. JavaScript/Lecture 08 (20-03-2021)/Code/raw/poc/potm(playerOfTheMatch).js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+let url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595/match-results";
+let request = require("request");
+let cheerio = require("cheerio");
+
+request(url,cb); 
+function cb(error,response,html){
+    if(error){
+        console.log(error);
+    }else{
+        extractHtml(html) 
+    }
+}
+
+function extractHtml(html){
+    let selectorTool = cheerio.load(html);
+    let matchCard = selectorTool(".col-md-8.col-16");
+
+    for(let i=0; i<matchCard.length; i++){
+        let cardsButtons = selectorTool(matchCard[i]).find(".btn.btn-sm.btn-outline-dark.match-cta");
+        let linkMatch = selectorTool(cardsButtons[2]).attr("href");
+        // console.log(linkMatch);
+
+        let fullLink = "https://www.espncricinfo.com" + linkMatch;
+        // console.log(fullLink);
+
+        printPOTM(fullLink);
+    }
+}
+
+function printPOTM(fullLink){
+    request(fullLink,cb);
+    function cb(error,response,fullLink){
+        if(error){
+            console.log(error);
+        }else{
+            extractPOTM(fullLink);
+        }
+    }
+}
+
+function extractPOTM(fullLink){
+    let selectorTool = cheerio.load(fullLink);
+    let potm_name = selectorTool(".best-player-content").text();
+    console.log(potm_name);
+}
