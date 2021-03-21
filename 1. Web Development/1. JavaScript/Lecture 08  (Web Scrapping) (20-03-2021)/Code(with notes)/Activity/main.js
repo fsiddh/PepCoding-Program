f2b5bf8d91de13aad60fbaf852c2b33b8b976341
fsiddh <jasonshaw546@gmail.com>
2021-03-21 08:33:52 +0530

    notes for main added

--- 1. Web Development/1. JavaScript/Lecture 08  (Web Scrapping) (20-03-2021)/Code(with notes)/Activity/main.js
+++ 1. Web Development/1. JavaScript/Lecture 08  (Web Scrapping) (20-03-2021)/Code(with notes)/Activity/main.js
@@ -4,7 +4,7 @@ const cheerio = require("cheerio");
 
 console.log("Before");
 
-request(url, cb);
+request(url, cb); // requests for whole html content of "url"
 function cb(error, response, html) {
     if (error) {
         console.log(error)
@@ -14,15 +14,16 @@ function cb(error, response, html) {
     }
 }
 
-// topic names and their resp. links
+// extracting topic names and their resp. links
 function extractHtml(html){
     let selTool = cheerio.load(html);
-    let allTopics = selTool(".col-12.col-sm-6.col-md-4.mb-4");
+    let allTopics = selTool(".col-12.col-sm-6.col-md-4.mb-4"); // extarcts all 3 topics
     // console.log(allTopics.length);
 
-    for(let i=0; i<allTopics.length; i++){
-        let currentTopicData = selTool(allTopics[i]).find(".f3.lh-condensed.text-center.Link--primary.mb-0.mt-1");
-        let currentTopicName = selTool(currentTopicData).text().trim();
+    // One by one looping through each topic and then extracting it's name and it's link from "a" tag.
+    for(let i=0; i<allTopics.length; i++){ 
+        let currentTopicData = selTool(allTopics[i]).find(".f3.lh-condensed.text-center.Link--primary.mb-0.mt-1"); // apn ne find use isliye kara kyoki apnko koi specific data chahie tha "allTopics" ke andr se! 
+        let currentTopicName = selTool(currentTopicData).text().trim(); // removes extra space from start and end.
 
         let currentTopicLink = selTool(allTopics[i]).find(".no-underline.d-flex.flex-column.flex-justify-center").attr("href");
         let fullLink = "https://github.com" + currentTopicLink;
@@ -35,6 +36,7 @@ function extractHtml(html){
     }
 }
 
+// Extracts whole html data of "allTopics[i]"th Topic
 function navigateToRepoHelper(topic, fullLink){
     request(fullLink, function(error, response, html){
         if(error){
@@ -46,12 +48,14 @@ function navigateToRepoHelper(topic, fullLink){
     })
 }
 
+// Extracts link of top 8 repos of "allTopics[i]"th Topic
 function navigateToRepo(topic, html){
     let selTool = cheerio.load(html);
-    let allRepo = selTool(".d-flex.flex-justify-between.my-3");
+    let allRepo = selTool(".d-flex.flex-justify-between.my-3"); // Extracts all repos
 
     // console.log(allRepo.length);
 
+    // finds top 8 repo links
     for(let i=0; i<9; i++){
         let repoLink = selTool(allRepo[i]).find(".text-bold").attr("href");
         console.log("https://github.com" + repoLink);
