9f1799a7114ec569896f59315d667f489b768eb4
fsiddh <jasonshaw546@gmail.com>
2021-03-21 09:58:23 +0530

    main update

--- 1. Web Development/1. JavaScript/Lecture 09 (Web Scrapping) (Main Acitivty - GitHub Topic Issues Scrapper) (21-03-2021)/Code/activity/main.js
+++ 1. Web Development/1. JavaScript/Lecture 09 (Web Scrapping) (Main Acitivty - GitHub Topic Issues Scrapper) (21-03-2021)/Code/activity/main.js
@@ -4,18 +4,49 @@ const cheerio = require("cheerio");
 
 console.log("Before");
 
-request(url, cb);
-function cb(error, response, html) {
+request(url, function(error, response, html) {
     if (error) {
         console.log(error)
     } else {
         // console.log(html);
-        extractHtml(html);
+        extractTopicNames(url, html);
     }
+    })
+
+function extractTopicNames(url, html){
+    let selTool = cheerio.load(html);
+    let allTopics = selTool(".col-12.col-sm-6.col-md-4.mb-4");
+    // console.log(allTopics.length);
+    let topicNamesArray = [];
+
+    for(let i=0; i<allTopics.length; i++){
+        let currentTopicData = selTool(allTopics[i]).find(".f3.lh-condensed.text-center.Link--primary.mb-0.mt-1");
+        let currentTopicName = selTool(currentTopicData).text().trim();
+
+        topicNamesArray.push(currentTopicName);
+    }
+
+    // console.log(topicNamesArray);
+    extractHtmlHelper(url, topicNamesArray)
+
+}
+
+function extractHtmlHelper(url, topicNamesArray) {
+    request(url, function(error, response, html){
+        if (error) {
+            console.log(error);
+        } else {
+            // console.log(html);
+            extractHtml(html, topicNamesArray);
+        }
+    })
 }
 
 // topic names and their resp. links
-function extractHtml(html){
+function extractHtml(html, topicNamesArray){
+
+    // console.log(topicNamesArray);
+
     let selTool = cheerio.load(html);
     let allTopics = selTool(".col-12.col-sm-6.col-md-4.mb-4");
     // console.log(allTopics.length);
@@ -31,22 +62,22 @@ function extractHtml(html){
         // console.log(fullLink);
         // console.log();
 
-        navigateToRepoHelper(currentTopicName, fullLink);
+        navigateToTopicHelper(currentTopicName, fullLink, topicNamesArray);
     }
 }
 
-function navigateToRepoHelper(topic, fullLink){
+function navigateToTopicHelper(topicName, fullLink, topicNamesArray){
     request(fullLink, function(error, response, html){
         if(error){
             console.log(error);
         }
         else{
-            navigateToRepo(topic, html);
+            navigateToTopic(topicName, html, topicNamesArray);
         }
     })
 }
 
-function navigateToRepo(topic, html){
+function navigateToTopic(html, topicName, topicNamesArray){
     let selTool = cheerio.load(html);
     let allRepo = selTool(".d-flex.flex-justify-between.my-3");
 
@@ -54,11 +85,56 @@ function navigateToRepo(topic, html){
 
     for(let i=0; i<9; i++){
         let repoLink = selTool(allRepo[i]).find(".text-bold").attr("href");
-        console.log("https://github.com" + repoLink);
+        // console.log("https://github.com" + repoLink);
+        let fullRepoLink = "https://github.com" + repoLink;
+        
+        navigateToRepoHelper(topicName, fullRepoLink, topicNamesArray);
     }
-    console.log();
+    // console.log();
 }
 
+function navigateToRepoHelper(topicName, fullRepoLink, topicNamesArray){
+    request(fullRepoLink, function(err, response, html){
+        if(err){
+            console.log(err);
+        }
+        else{
+            navigateToRepo(topicName, html, topicNamesArray);
+        }
+    })
+}
+
+function navigateToRepo(html, topicName, topicNamesArray){
+    let selTool = cheerio.load(html);
+    
+    let allfuncIncludingIssues = selTool(".UnderlineNav-body.list-style-none .d-flex");
+    let issueLink = selTool(allfuncIncludingIssues[1]).find("a").attr("href").trim();
+    let fullIssueLink = "https://github.com" + issueLink;
+
+    console.log(fullIssueLink);
+    // console.log(topicName + ": " + fullIssueLink);
+
+    // navigateToIssueHelper(fullIssueLink, topicName, topicNamesArray);
+}
+
+function navigateToIssueHelper(fullIssueLink, topicName, topicNamesArray){
+    request(fullIssueLink, function(err, response, html){
+        if(err){
+            console.log(err);
+        }
+        else{
+            navigateToIssue(html, topicName, topicNamesArray);
+        }
+    })
+}
+
+function navigateToIssue(html, topicName, topicNamesArray){
+    let selTool = cheerio.load(html);
+
+    let allIssues = selTool(".Box-row.Box-row--focus-gray.p-0.mt-0.js-navigation-item.js-issue-row");
+    // console.log(allIssues.length);
+
+}
 
 
 console.log("After");
