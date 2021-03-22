7eba7ed1a043c946750b0783c3302552f9b12b35
fsiddh <jasonshaw546@gmail.com>
2021-03-22 08:37:16 +0530

    main Activity code with notes

--- 1. Web Development/1. JavaScript/Lecture 09 (Web Scrapping) (Main Acitivty - GitHub Topic Issues Scrapper) (21-03-2021)/Code(with notes)/Activity/main.js
+++ 1. Web Development/1. JavaScript/Lecture 09 (Web Scrapping) (Main Acitivty - GitHub Topic Issues Scrapper) (21-03-2021)/Code(with notes)/Activity/main.js
@@ -1,68 +1,152 @@
-const url = "https://github.com/topics";
-const request = require("request");
-const cheerio = require("cheerio");
+let url = "https://github.com/topics";
+let request = require("request");
+let cheerio = require("cheerio");
+let fs = require("fs");
+let path = require("path");
+let pdfDocument = require("pdfkit"); //module for creating pdf
 
-console.log("Before");
-
-request(url, cb); // requests for whole html content of "url"
-function cb(error, response, html) {
-    if (error) {
-        console.log(error)
-    } else {
-        // console.log(html);
-        extractHtml(html);
+//creating folder of TOPIC
+function dirCreator(topicName){
+    let pathOfFolder = path.join(__dirname, topicName);
+    if(fs.existsSync(topicName) == false){
+        fs.mkdirSync(pathOfFolder);
     }
 }
 
-// extracting topic names and their resp. links
-function extractHtml(html){
-    let selTool = cheerio.load(html);
-    let allTopics = selTool(".col-12.col-sm-6.col-md-4.mb-4"); // extarcts all 3 topics
-    // console.log(allTopics.length);
-
-    // One by one looping through each topic and then extracting it's name and it's link from "a" tag.
-    for(let i=0; i<allTopics.length; i++){ 
-        let currentTopicData = selTool(allTopics[i]).find(".f3.lh-condensed.text-center.Link--primary.mb-0.mt-1"); // apn ne find use isliye kara kyoki apnko koi specific data chahie tha "allTopics" ke andr se! 
-        let currentTopicName = selTool(currentTopicData).text().trim(); // removes extra space from start and end.
-
-        let currentTopicLink = selTool(allTopics[i]).find(".no-underline.d-flex.flex-column.flex-justify-center").attr("href");
-        let fullLink = "https://github.com" + currentTopicLink;
-
-        // console.log(currentTopicName);
-        // console.log(fullLink);
-        // console.log();
-
-        navigateToRepoHelper(currentTopicName, fullLink);
+//creating .json files of repositories
+function fileCreator( repoName,topicName){
+    let filePath = path.join(__dirname, topicName, repoName + ".json");
+    if(fs.existsSync(filePath) == false){ //method of creating file
+        let file = fs.createWriteStream(filePath); 
+        file.end();
     }
 }
 
-// Extracts whole html data of "allTopics[i]"th Topic
-function navigateToRepoHelper(topic, fullLink){
-    request(fullLink, function(error, response, html){
+//all issues name and link of a particular repo
+function extractRepoIssues(html,topicName,repoName){
+    let selectorTool = cheerio.load(html);
+    let issues = selectorTool(".Link--primary.v-align-middle.no-underline.h4.js-navigation-open.markdown-title"); // Extracted all Issues anchor tags
+    let issueObj = []; // To store objects of each Issue eg-> [{"Name":issueName, "Link":issueLink}, {...}, {...}, {...}]
+
+    for(let i=0;i<issues.length;i++){
+        let issueName = selectorTool(issues[i]).text();
+        let issueLink = "https://github.com" + selectorTool(issues[i]).attr("href");
+        // console.log(topicName+":"+issueName+":"+issueLink);
+
+        let obj = {
+            "Name" : issueName,
+            "Link" : issueLink,
+        }
+        issueObj.push(obj);
+    }
+    // console.log(topicName);
+    // console.table(issueObj);
+    // console.log("========================");
+
+    // ==========================================JSON Creation=============================================================
+    let file_path = path.join(__dirname, topicName, repoName + ".json");
+    fs.writeFileSync(file_path, JSON.stringify(issueObj));
+
+
+    //==========================================PDF CREATION===============================================================
+    // let filePath = path.join(__dirname,topicName,repoName+".pdf");
+    // let pdfDoc = new pdfDocument;
+    // pdfDoc.pipe(fs.createWriteStream(filePath));
+    // pdfDoc.text(JSON.stringify(issueObj));
+    // pdfDoc.end();
+}
+
+function getallIssues(issueLink,topicName,repoName){
+    request(issueLink, function(error,response,html){
+        if(error){
+            // if(response.status== 404){
+            //     console.log("No issue page found");
+            // }
+            // else{
+            console.log(error);
+            //}
+        }
+        else{
+            extractRepoIssues(html,topicName,repoName);
+        }
+    });
+}
+
+//issue page link via its repolink html
+function extractIssue(html,topicName,repoName){ // page -> github.com/topics/topicName/repoName
+    let selectorTool = cheerio.load(html);
+
+    let allLinks = selectorTool(".UnderlineNav-body.list-style-none .d-flex");
+    let issueLink = selectorTool(allLinks[1]).find("a"); // 2nd tab -> issue anchor tag
+    issueLink = "https://github.com" + selectorTool(issueLink).attr("href"); // full link to issue page
+
+    getallIssues(issueLink,topicName,repoName);
+    // console.log(issueLink," : ",topicName);
+}
+
+function getIssue(repositoryLink,topicName,repoName){
+    request(repositoryLink, function(error,response,html){
         if(error){
             console.log(error);
         }
         else{
-            navigateToRepo(topic, html);
+            extractIssue(html,topicName,repoName);
         }
-    })
+    });
 }
 
-// Extracts link of top 8 repos of "allTopics[i]"th Topic
-function navigateToRepo(topic, html){
-    let selTool = cheerio.load(html);
-    let allRepo = selTool(".d-flex.flex-justify-between.my-3"); // Extracts all repos
+//8 repo links for each topic html
+function extractRepoLink(html){ // page -> github.com/topics/topicName
+    let selectorTool = cheerio.load(html);
+    let topicName = selectorTool(".h1-mktg").text().trim();  //Extacted Topic name & trimmed it because it had many spaces
 
-    // console.log(allRepo.length);
+    // console.log(topicName);
 
-    // finds top 8 repo links
-    for(let i=0; i<9; i++){
-        let repoLink = selTool(allRepo[i]).find(".text-bold").attr("href");
-        console.log("https://github.com" + repoLink);
+    dirCreator(topicName); //creating folder named based on "topicName"
+
+    let allRepos = selectorTool(".border.rounded.color-shadow-small.color-bg-secondary.my-4"); 
+
+    for(let i=0;i<8;i++){
+        let allATag = selectorTool(allRepos[i]).find("a");   // got anchortags 
+        let repositoryLink = selectorTool(allATag[1]).attr("href");   // selected 2nd anchor tag (repo link)
+        let repoName = repositoryLink.split("/").pop();  // eg -> kc0112/Algorithms (repoLink)  ->  Algorithms (repoName)
+        repoName = repoName.trim(); // removed spaces
+
+        fileCreator(repoName, topicName); // create file on path "./topicName/repoName.json"
+
+        repositoryLink = "https://github.com" + repositoryLink; // full link
+        getIssue(repositoryLink,topicName,repoName); 
     }
-    console.log();
+}
+
+function getRepoHelper(link){
+    request(link,cb);
+    function cb(error,response,html){
+        if(error){
+            console.log(error);
+        }else{
+            extractRepoLink(html); 
+        }
+    }
+}
+
+// Extracts 3 topic links via github.com/topics html
+function extractTopicLink(html){ // page -> github.com/topics
+    let selectorTool = cheerio.load(html);
+    let topics= selectorTool(".col-12.col-sm-6.col-md-4.mb-4 a");
+    for(let i=0; i<topics.length; i++){
+        let topiclink = "https://github.com" + selectorTool(topics[i]).attr("href");   // got topic link page
+        getRepoHelper(topiclink);
+    }
+}
+
+request(url,callback); // callback is a function passed into another function as an argument to be executed later
+function callback(error,response,html){
+    if(error){
+        console.log(error);
+    }else{
+        extractTopicLink(html) ;
+    }
 }
 
 
-
-console.log("After");
