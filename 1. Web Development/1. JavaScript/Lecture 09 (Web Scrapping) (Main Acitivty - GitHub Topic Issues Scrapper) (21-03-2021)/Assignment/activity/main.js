53b4bc613c8e3ea2b3034878914169179178352c
fsiddh <jasonshaw546@gmail.com>
2021-03-22 21:01:02 +0530

    assignmet done

--- 1. Web Development/1. JavaScript/Lecture 09 (Web Scrapping) (Main Acitivty - GitHub Topic Issues Scrapper) (21-03-2021)/Assignment/activity/main.js
+++ 1. Web Development/1. JavaScript/Lecture 09 (Web Scrapping) (Main Acitivty - GitHub Topic Issues Scrapper) (21-03-2021)/Assignment/activity/main.js
@@ -2,11 +2,20 @@ const cheerio = require("cheerio");
 const request = require("request");
 const fs = require("fs");
 const path = require("path");
+const iplFolderName = "ipl_2020";
 
 const url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595/match-results";
 
+function iplDir(){
+    let folderPath = path.join(__dirname, iplFolderName);
+
+    if(fs.existsSync(folderPath) == false){
+        fs.mkdirSync(folderPath);
+    }
+}
+
 function createDir(teamName){
-    let folderPath = path.join(__dirname, teamName);
+    let folderPath = path.join(__dirname, iplFolderName, teamName);
 
     if(fs.existsSync(folderPath) == false){
         fs.mkdirSync(folderPath);
@@ -14,12 +23,48 @@ function createDir(teamName){
 }
 
 function createJSON(teamName, batsmenName){
-    let filePath = path.join(__dirname, teamName, batsmenName + ".json");
+    let filePath = path.join(__dirname, iplFolderName, teamName, batsmenName + ".json");
 
     if(fs.existsSync(filePath) == false){
         let file = fs.createWriteStream(filePath);
         file.end();
     }
+}
+
+function fillJsonWithStats(batsmanTeam, opponentTeam, batsmanName, currentTeam_batsmenRow, selTool){
+    let objArr = [];
+    
+    let batsmanRow = selTool(currentTeam_batsmenRow).find("td");
+    // console.log(batsmanRow.length);
+
+    let opponentName = opponentTeam;
+    let runs = selTool(batsmanRow[2]).text();
+    let balls = selTool(batsmanRow[3]).text();
+    let fours = selTool(batsmanRow[5]).text();
+    let sixes = selTool(batsmanRow[6]).text();
+    let sr = selTool(batsmanRow[7]).text();
+
+    let description = selTool(".match-info.match-info-MATCH .description").text().split(",");
+    let date = description[2];
+    let venue = description[1];
+    // console.log(description);
+
+    let result = selTool(".match-info.match-info-MATCH .status-text").text();
+
+    objArr.push({
+        "opponentName": opponentName,
+        "runs": runs,
+        "balls": balls,
+        "fours": fours,
+        "sixes": sixes,
+        "sr": sr,
+        "date": date,
+        "venue": venue,
+        "result": result
+    })
+
+    let file_path = path.join(__dirname, iplFolderName, batsmanTeam, batsmanName + ".json");
+    fs.writeFileSync(file_path, JSON.stringify(objArr));
 }
 
 function gotMatchLinkHTML(html){
@@ -37,15 +82,22 @@ function gotMatchLinkHTML(html){
 
     let batsmenContainer = selTool(".table.batsman"); // get both team batsmen tables
     for(let i=0; i<batsmenContainer.length; i++){
-        let team1_batsmen = selTool(batsmenContainer[i]).find("tbody tr"); // for every table get all batsmeen data
+        let currentTeam_batsmen = selTool(batsmenContainer[i]).find("tbody tr"); // for every table get all batsmeen data
         
-        for(let j=0; j<team1_batsmen.length; j+=2){ // get relevant batsmen row
-            let batsmenAnchor = selTool(team1_batsmen[j]).find("a");
+        for(let j=0; j<currentTeam_batsmen.length-1; j+=2){ // get relevant batsmen row
+            let batsmenAnchor = selTool(currentTeam_batsmen[j]).find("a");
             
-            let batsmenName = selTool(batsmenAnchor).text();
-            let batsmenLink = selTool(batsmenAnchor).attr("href");
+            let batsmanName = selTool(batsmenAnchor).text().trim();
+            // let batsmenLink = selTool(batsmenAnchor).attr("href");
 
-            createJSON(selTool(bothTeams[i]).text(), batsmenName);
+            createJSON(selTool(bothTeams[i]).text(), batsmanName);
+
+            if(i == 0){
+                fillJsonWithStats(selTool(bothTeams[i]).text(), selTool(bothTeams[1]).text(), batsmanName, currentTeam_batsmen[j], selTool);
+            }
+            else{
+                fillJsonWithStats(selTool(bothTeams[i]).text(), selTool(bothTeams[0]).text(), batsmanName, currentTeam_batsmen[j], selTool);
+            }
         }
     }
 }
@@ -64,6 +116,8 @@ function getMatchLinkHTML(fullLink){
 function getAllMatches(html){
     let selTool = cheerio.load(html);
     let allMatches = selTool(".match-info-link-FIXTURES"); // anchor tag of all matches( ie 60!)
+
+    iplDir();
 
     for(let i=0; i<allMatches.length; i++){
         let matchLink = selTool(allMatches[i]).attr("href");
