a176ca28971999c0af632545336011b07907bfd5
fsiddh <jasonshaw546@gmail.com>
2021-03-27 15:40:33 +0530

    login code update

--- 1. Web Development/1. JavaScript/Lecture 11 (Automation) (Promises) (Hackerrank Activity - Log Into using puppeteer) (27-03-2021)/Code/Activity/hackerrank_Automate_Login.js
+++ 1. Web Development/1. JavaScript/Lecture 11 (Automation) (Promises) (Hackerrank Activity - Log Into using puppeteer) (27-03-2021)/Code/Activity/hackerrank_Automate_Login.js
@@ -1,26 +1,28 @@
-let puppeteer = require("puppeteer");
-let {email, password} = require("../../secrets");
-let global_tab;
+let puppeteer = require("puppeteer"); 
+let {email, password} = require("../../secrets"); // imported email & paswrd
+let global_tab; // to keep info of TAB we'll work on
 
 console.log("before");
 
+// Launched Browser 
 let browserPromise = puppeteer.launch({
-    headless: false
+    headless: false // ":false" s Automation hote hue dikhta hai
 })
 
 browserPromise
-    .then(function(browserReference){
-        let newTabPromise = browserReference.newPage();
-        return newTabPromise;
+    .then(function(browserReference){ // after launch -> we'll get browser's refrnce
+        let newTab_Promise = browserReference.newPage(); // ".newPage()"(returns promise) for opening new TAB
+        return newTab_Promise;  // Necessary to return for Chained Promises! 
     }).then(function(newTabReference){
         global_tab = newTabReference;
         
-        let loginPageWillBeOpenedPromise = newTabReference.goto("https://www.hackerrank.com/auth/login?h_l=body_middle_left_button&h_r=login");
-        return loginPageWillBeOpenedPromise;
+        let loginPageWillBeOpened_Promise = newTabReference.goto("https://www.hackerrank.com/auth/login?h_l=body_middle_left_button&h_r=login");
+        return loginPageWillBeOpened_Promise;
     }).then(function(){
         console.log("Login Page Opened");
 
-        let emailWillBeTyped_Promise = global_tab.type("#input-1", email, {delay: 200});
+        // type -> Types into a selector that identifies a form element
+        let emailWillBeTyped_Promise = global_tab.type("#input-1", email, {delay: 200}); // type(tag, email id , delay rate for typing)
         return emailWillBeTyped_Promise;
     }).then(function(){
         console.log("email Entered!");
@@ -30,7 +32,7 @@ browserPromise
     }).then(function(){
         console.log("password Entered");
 
-        let loginButtonWillBeClicked_Promise = global_tab.click(".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled");
+        let loginButtonWillBeClicked_Promise = global_tab.click(".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled"); // Perform a mouse click event on the element passed as parameter
         return loginButtonWillBeClicked_Promise;
     }).then(function(){
         console.log("Login Completed");
