bae7c5c72ada1bab275dff29bfae604ca3a56756
fsiddh <jasonshaw546@gmail.com>
2021-03-28 12:34:13 +0530

    promise code notes added

--- 1. Web Development/1. JavaScript/Lecture 11 (Automation) (Promises) (Hackerrank Activity - Log Into using puppeteer) (27-03-2021)/Code( With Notes )/raw/facts/fsread.js
+++ 1. Web Development/1. JavaScript/Lecture 11 (Automation) (Promises) (Hackerrank Activity - Log Into using puppeteer) (27-03-2021)/Code( With Notes )/raw/facts/fsread.js
@@ -1,14 +1,16 @@
 const fs = require("fs");
 console.log("before");
 
-let promise = fs.promises.readFile("f1.txt");
+let promise = fs.promises.readFile("f1.txt"); // this ".rF" is an "promises" method which work similar to a callback fn() ie
+                                              // Asynchronously
 
-console.log("Initial State: ", promise);
-promise.then(function (data) {
-    console.log(data+"");
+console.log("Initial State: ", promise); // Initial state of a "promise" is always pending
+
+promise.then(function (data) { // ".then" executes when the promise is completed or resolved!
+    console.log(data+""); 
 })
 
-promise.catch(function(err){
+promise.catch(function(err){ // ".catch" executes when the promise is rejected or some error has occured!
     console.log("err", err);
 })
 
