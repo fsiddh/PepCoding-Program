f86e6e887ec69c2cb53c84274ae4648040a9edb9
fsiddh <jasonshaw546@gmail.com>
2021-04-10 16:07:38 +0530

    updates

--- 1. Web Development/1. JavaScript/Lecture 15 (Automation) (More about promises) (hkRank Activity cont. - submitted all codes) (10-04-2021)/Code/raw/poc/puppeteer.js
+++ 1. Web Development/1. JavaScript/Lecture 15 (Automation) (More about promises) (hkRank Activity cont. - submitted all codes) (10-04-2021)/Code/raw/poc/puppeteer.js
@@ -1,15 +1,12 @@
 let puppeteer = require("puppeteer");
 
-// To launch Browser
 let browserWillBeLaunchedPromise = puppeteer.launch({ 
     headless: false
 })
 
-// Jab browser launch ho jaega tab .then will execute
-// it's function and pass launched browser's refernce 
 browserWillBeLaunchedPromise
     .then(function(browserInstance){
-        let newPagePromise = browserInstance.newPage(); // for new tab
+        let newPagePromise = browserInstance.newPage(); 
         
         newPagePromise
             .then(function(newPage){
