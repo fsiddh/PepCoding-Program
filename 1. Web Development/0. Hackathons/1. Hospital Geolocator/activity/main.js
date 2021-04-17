6aa16dceb7d35cfd5136a57f29217fd73708f349
fsiddh <jasonshaw546@gmail.com>
2021-04-17 21:19:40 +0530

    main script added

--- 1. Web Development/0. Hackathons/1. Hospital Geolocator/activity/main.js
+++ 1. Web Development/0. Hackathons/1. Hospital Geolocator/activity/main.js
@@ -1,18 +1,49 @@
 const puppeeteer = require("puppeteer");
+const path = require("path");
+const fs = require("fs");
+
 const hospitalFnObj = require("./Utilities/hospital.js");
+const gMapsFnObj = require("./Utilities/gmaps.js")
+const mailFnObj = require("./Utilities/mail.js")
 const { email, password } = require("../auth_details.js");
 
+let recieverEmail = process.argv[2];
+
+function infoDir(){
+    let folderPath = path.join(__dirname, "Utilities", "Information");
+
+    if(fs.existsSync(folderPath) == false){
+        fs.mkdirSync(folderPath);
+    }
+}
+
 (async function () {
-	try {
+    try {
+        // Create Information Directory
+        infoDir();
+
+        // Launch Browser
 		let browserRef = await puppeeteer.launch({
 			headless: false,
 			defaultViewport: null,
 			args: ["--start-maximized"],
-		});
+        });
+        
+        // Extract Information on nearby Hospital 
+        let hospital_detailsObj = await hospitalFnObj.hospitalFn(browserRef);
+        console.table(hospital_detailsObj);
 
-        let x = await hospitalFnObj.hospitalFn(browserRef);
-        console.log(x);
-    } catch (error) {
-        console.log(error)
-    }
+        // Extract nearby Hospital route details
+        // let gMaps_link = await gMapsFnObj.gMapsFn(browserRef, hospital_detailsArr.Name);
+        let gMaps_infoObj = await gMapsFnObj.gMapsFn(browserRef, hospital_detailsObj.Name, hospital_detailsObj.Address);
+        console.log(gMaps_infoObj);
+
+        // Send Mail
+        // let temp_recieverEmail = "jasonshaw546@gmail.com"
+        await mailFnObj.mailFn(browserRef, recieverEmail);
+
+        browserRef.close();
+	} catch (error) {
+		console.log(error);
+	}
 })();
