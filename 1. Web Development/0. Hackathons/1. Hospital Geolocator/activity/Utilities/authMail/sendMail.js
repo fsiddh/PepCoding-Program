4837be88aac5f2a865e6a462167ccf9fc100f8fb
fsiddh <jasonshaw546@gmail.com>
2021-04-18 20:12:43 +0530

    mailSend completed via outlook hotmail

---
+++ 1. Web Development/0. Hackathons/1. Hospital Geolocator/activity/Utilities/authMail/sendMail.js
@@ -0,0 +1,44 @@
+const nodemailer = require("nodemailer");
+const fs = require("fs");
+const temp_recieverEmail = "testing10200201@outlook.com";
+
+async function gmailsend(recieverEmail) {
+	let data = await fs.promises.readFile("auth_details.json", "utf-8");
+	let credentials = JSON.parse(data);
+	// console.log(credentials);
+
+	email = credentials.email;
+	password = credentials.password;
+
+	let transport = nodemailer.createTransport({
+		service: "hotmail",
+		auth: {
+			user: email,
+			pass: password,
+		},
+	});
+	let message = {
+		from: email,
+		to: recieverEmail,
+		subject: "reg: Hospital Details",
+		text: "Hospital details along with its Images.",
+		attachments: [
+			{
+				filename: "test_file.txt",
+				path: `./test_file.txt`,
+			},
+		],
+	};
+	transport.sendMail(message, function (err) {
+		if (err) {
+			console.log("Failed to send email.\n" + err.message);
+			return;
+		}
+		console.log(`Email sent to ${recieverEmail} \ncheck your email.`);
+		return "Done";
+	});
+}
+
+module.exports = {
+	gmailsendFn: gmailsend,
+};
