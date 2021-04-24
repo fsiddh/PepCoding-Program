53e4254163451d013a1070103422c45cfaf123e5
fsiddh <jasonshaw546@gmail.com>
2021-04-24 12:15:14 +0530

    updates

--- 1. Web Development/2. Browser/Lecture 01 (Jira App) (ToDo app) (DOM , html, css) (23-04-2021)/Code/ToDo/script.js
+++ 1. Web Development/2. Browser/Lecture 01 (Jira App) (ToDo app) (DOM , html, css) (23-04-2021)/Code/ToDo/script.js
@@ -1,20 +1,25 @@
+//  dom node
 let input = document.querySelector(".input_box");
-let ul = document.querySelector(".class_list");
-
+let ul = document.querySelector(".task-list");
+// let li = document.querySelector(".task");
 input.addEventListener("keydown", function (e) {
-    console.log("Event Object: ", e);
-    if (e.key == "Enter") {
-        let task = input.value;
-        let li = document.createElement("li");
-        li.innerText = task;
-        
-        li.addEventListener("dblclick", function () {
-            li.remove();
-        })
+	//  e object -> describe -> event
+	// console.log(" some key was pressed");
+	console.log("event object", e);
+	if (e.key == "Enter") {
+		// console.log("user want to enter a task");
 
-        li.setAttribute("class", "task");
-        ul.appendChild(li);
-
-        input.value = "";
-    }
-})
+		let task = input.value;
+		//  create any html tag
+		// console.log(task);
+		let li = document.createElement("li");
+		li.innerText = task;
+		li.addEventListener("dblclick", function (e) {
+			li.remove();
+		});
+		//  add any attribute
+		li.setAttribute("class", "task");
+		ul.appendChild(li);
+		input.value = "";
+	}
+});
