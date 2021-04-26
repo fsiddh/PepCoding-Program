c89d5b504328e5338051a334c1c40ab67491a3fd
fsiddh <jasonshaw546@gmail.com>
2021-04-26 11:36:43 +0530

    till adding basic js

--- 1. Web Development/2. Browser/Lecture 02 (Jira App) (border box, inline+block, media query, display flex) (24-04-2021)/Code/activity/script.js
+++ 1. Web Development/2. Browser/Lecture 02 (Jira App) (border box, inline+block, media query, display flex) (24-04-2021)/Code/activity/script.js
@@ -1,1 +1,9 @@
-let color
+let colorBtn = document.querySelectorAll(".filter_color");
+let mainContainer = document.querySelector(".main-container");
+
+for (let i = 0; i < colorBtn.length; i++) {
+    colorBtn[i].addEventListener("click", function (e) {
+        let color = colorBtn[i].classList[1];
+        mainContainer.style.backgroundColor = color;
+    })
+}
