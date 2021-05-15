d951f263172ee4e765a8381687496f8f3057e9c1
fsiddh <jasonshaw546@gmail.com>
2021-05-15 11:45:55 +0530

    formula Bar udates

--- 1. Web Development/2. Browser/Lecture 09 (Browser Excel) (Formula Bar) (09-05-2021)/Code/activity/formulaBar.js
+++ 1. Web Development/2. Browser/Lecture 09 (Browser Excel) (Formula Bar) (09-05-2021)/Code/activity/formulaBar.js
@@ -1,23 +1,26 @@
 // ====FORMULA BAR FUNCTIONS======================================================================================================
 
+// eg taken => C1 = " A1 + B1 "
+//                    10 + 20
 // There are 4 Cases in FORMULA BAR:
 
 // Case 1: Value to Value
 // Case 2: Formula to Value
+//		   => if FtV -> parent k child me se hatao + formula hatao (removeFormula)
+//		   => DB value change
+//		   => C1 k children pr impact dekho (changeChildren)
 for (let i = 0; i < allCells.length; i++) {
 	allCells[i].addEventListener("blur", function () {
-		let address = addressBar.value;
-		let { rid, cid } = getRIdCIdfromAddress(address);
+		let address = addressBar.value; //A1
+		let { rid, cid } = getRIdCIdfromAddress(address); //00
 		let cell = document.querySelector(`.col[rid="${rid}"][cid="${cid}"]`);
-		
+
 		let cellObject = sheetDB[rid][cid];
-		
-		console.log(cellObject.value, cell.innerText);
 
 		if (cellObject.value == cell.innerText) {
 			return;
 		}
-		
+
 		if (cellObject.formula != "") {
 			removeFormula(cellObject, address);
 		}
@@ -30,12 +33,18 @@ for (let i = 0; i < allCells.length; i++) {
 
 // Case 3: Value to Formula
 // Case 4: Formula to Formula
+// 		   => new formula input se lo
+// 		   => if FtF -> parent k child me se hatao + formula hatao (removeFormula)
+// 		   => naye formula se calculate kro (evaluateFormula)
+// 		   => display calculated value (updateUI)
+// 		   => DB edits => value,formula,children (setFormula)
+// 		   => C1 k children pr impact dekho (changeChildren)
 formulaBarInput.addEventListener("keydown", function (e) {
 	if (e.key == "Enter" && formulaBarInput.value != "") {
 		let newFormula = formulaBarInput.value;
 
-		let address = addressBar.value;
-		let { rid, cid } = getRIdCIdfromAddress(address);
+		let address = addressBar.value; // C1
+		let { rid, cid } = getRIdCIdfromAddress(address); //00
 		let cellObject = sheetDB[rid][cid];
 		let oldFormula = cellObject.formula;
 
@@ -57,14 +66,16 @@ formulaBarInput.addEventListener("keydown", function (e) {
 
 // ====HELPING FUNCTIONS======================================================================================================
 
+// if C1 = " A1 + B1 "  => A1,B1 ke children me se C1 ko hatana
+//						=> cellObject.formula = "";
 function removeFormula(cellObject, address) {
-	let formula = cellObject.formula; // C1 = " A1 + B1 " 
+	let formula = cellObject.formula; // C1 = " A1 + B1 "
 	let formulaTokens = formula.split(" "); // ["A1", "+", "B1",]
 	for (let i = 0; i < formulaTokens.length; i++) {
 		let firstCharofToken = formulaTokens[i].charCodeAt(0);
 		if (firstCharofToken >= 65 && firstCharofToken <= 90) {
 			let parentAddress = formulaTokens[i]; // A1
-			let parentRIdCId = getRIdCIdfromAddress(parentAddress); 
+			let parentRIdCId = getRIdCIdfromAddress(parentAddress);
 			let parentCellObject = sheetDB[parentRIdCId.rid][parentRIdCId.cid];
 
 			let parentsChildren = parentCellObject.children;
@@ -75,11 +86,18 @@ function removeFormula(cellObject, address) {
 	cellObject.formula = "";
 }
 
+// C1 change to uske children affected ( eg. C1.children = D1)
+// for all children of C1
+//		=> go to child,evaluate their new value via formula
+//		=> display new value
+//		=> DB value change
+//		=> check for child's children now (recursion)
+//		=> repeat for next child
 function changeChildren(cellObject) {
-	let children = cellObject.children; // ["B1", "B2"]
+	let children = cellObject.children; // D1
 	console.log(children);
 	for (let i = 0; i < children.length; i++) {
-		let childAddress = children[i]; // "C1"
+		let childAddress = children[i]; // "A1"
 		let childRIdCId = getRIdCIdfromAddress(childAddress);
 		let childCellObject = sheetDB[childRIdCId.rid][childRIdCId.cid];
 		let childCellFormula = childCellObject.formula;
@@ -92,10 +110,11 @@ function changeChildren(cellObject) {
 	}
 }
 
+// returns ans calculated via formula
 function evaluateFormula(formula) {
-	// formula = "A1 + A2"
+	// formula =" A1 + B1 "
 
-	let formulaTokens = formula.split(" "); // ["(", "A1", "+", "A2", ")"]
+	let formulaTokens = formula.split(" "); // ["(", "A1", "+", "B1", ")"]
 
 	for (let i = 0; i < formulaTokens.length; i++) {
 		let firstCharofToken = formulaTokens[i].charCodeAt(0); // ex) "A1".charCodeAt(0) -> 65
@@ -106,20 +125,21 @@ function evaluateFormula(formula) {
 			let parentCellObject = sheetDB[rid][cid];
 			let { value } = parentCellObject;
 
-			formula = formula.replace(formulaTokens[i], value);
+			formula = formula.replace(formulaTokens[i], value); // A1 => A1 pr jo value
 		}
 	}
-	// formula = (10 + 20) = 30
-	let ans = eval(formula);
+	// formula = (10 + 20)
+	let ans = eval(formula); // 10 + 20 =30
 	return ans;
 }
 
+// display calculated value at C1
 function updateUI(evaluatedValue, rid, cid) {
-	document.querySelector(
-		`.col[rid="${rid}"][cid="${cid}"]`
-	).innerText = evaluatedValue;
+	document.querySelector(`.col[rid="${rid}"][cid="${cid}"]`).innerText =
+		evaluatedValue;
 }
 
+// DB edits => value,formula,children
 function setFormula(evaluatedValue, newFormula, rid, cid, address) {
 	let cellObject = sheetDB[rid][cid];
 	cellObject.value = evaluatedValue;
